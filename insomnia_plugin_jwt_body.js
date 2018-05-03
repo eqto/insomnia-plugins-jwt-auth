c4aa17b34accc90ee51f768a4e79e299110b7137
Tuxer <e.yulian@gmail.com>
2018-05-03 15:39:20 +0700

    init

---
+++ insomnia_plugin_jwt_body.js
@@ -0,0 +1,38 @@
+const crypto = require('crypto');
+
+module.exports.templateTags = [{
+    name: 'jwtbody',
+    displayName: 'JWT Body',
+    description: 'Generate JWT',
+    args: [
+        {
+            displayName: 'Payload',
+            type: 'string',
+            placeholder: 'Payload'
+        },
+        {
+            displayName: 'Secret',
+            type: 'string',
+            placeholder: 'Secret'
+        }
+    ],
+    run (context, payload, secret) {
+        var header = Buffer.from('{"alg": "HS256","typ": "JWT"}').toString('base64')
+        header = header.replace(/=+$/, '');
+        header = header.replace(/\+/g, '-');
+        header = header.replace(/\//g, '_');
+
+        var payload64 = Buffer.from(payload).toString('base64')
+        payload64 = payload64.replace(/=+$/, '');
+        payload64 = payload64.replace(/\+/g, '-');
+        payload64 = payload64.replace(/\//g, '_');
+
+        const hash = crypto.createHmac('sha256', secret);
+        signature = hash.update(header + '.' + payload64).digest('base64');
+        signature = signature.replace(/=+$/, '');
+        signature = signature.replace(/\+/g, '-');
+        signature = signature.replace(/\//g, '_');
+    
+        return header + '.' + payload64 + '.' + signature;
+    }
+}];
